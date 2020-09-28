890b7254d6b78f9be116e0133e41d14e17782f76
Andrew Ebling <andrew@tenero.co.uk>
2020-09-28 07:32:50 +0100

    Code cleanups - revert BPM label to placeholder value set in the storyboard when unsubscribing from heart rate characteristic.

--- HeartRevs/HeartRateMonitorViewController.swift
+++ HeartRevs/HeartRateMonitorViewController.swift
@@ -13,29 +13,39 @@ class HeartRateMonitorViewController: UIViewController {
     let notificationCenter = NotificationCenter.default
     var hrmReader: HRMReader?
     @IBOutlet weak var label: UILabel!
+    var placeholderText: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        notificationCenter.addObserver(forName: UIScene.willEnterForegroundNotification, object: nil, queue: nil) { (_) in
-            self.hrmReader = HRMReader(delegate: self)
-        }
-        
-        notificationCenter.addObserver(forName: UIScene.willDeactivateNotification, object: nil, queue: nil) { (_) in
-            self.hrmReader?.willDeactivate()
-            self.hrmReader = nil
-            self.label.text = "---"
-        }
+        placeholderText = self.label.text
+        observeNotifications()
+    }
+     
 
+    func show(_ error: String) {
+        present(errorMessage(error), animated: true, completion: nil)
     }
     
-    func show(_ error: String) {
+    private func errorMessage(_ error: String) -> UIViewController {
         let ac = UIAlertController(title: "Error",
                                    message: error,
                                    preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        return ac
+    }
+    
+    private func observeNotifications() {
         
-        present(ac, animated: true, completion: nil)
+        notificationCenter.addObserver(forName: UIScene.willEnterForegroundNotification, object: nil, queue: nil) { _ in
+            self.hrmReader = HRMReader(delegate: self)
+        }
+        
+        notificationCenter.addObserver(forName: UIScene.willDeactivateNotification, object: nil, queue: nil) { _ in
+            self.hrmReader?.willDeactivate()
+            self.hrmReader = nil
+            self.label.text = self.placeholderText
+        }
     }
 }
 
